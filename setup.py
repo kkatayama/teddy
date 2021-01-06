81af49e2a76af8b56f98cf80c4e6e7eabea77b97
kkatayama <katayama@udel.edu>
2021-01-06 12:54:50 -0500

    fix MODULE name parsing: os.path.splittext(MODULE)[0]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='teddy',
-      version='0.71',
+      version='0.72',
       description='Teddy Utilities',
       url='http://github.com/kkatayama/teddy',
       author='Teddy',
--- teddy/__init__.py
+++ teddy/__init__.py
@@ -19,7 +19,7 @@ from math import factorial as mf
 
 # -- CONFIGS -- #
 MODULE = coloredlogs.find_program_name()
-LOG_FILE = '{}.log'.format(os.path.splitext(MODULE))
+LOG_FILE = '{}.log'.format(os.path.splitext(MODULE)[0])
 field_styles = {
     'asctime': {'color': 221, 'bright': True},
     'programname': {'color': 45, 'faint': True},
