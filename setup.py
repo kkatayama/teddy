3b73e01986bb17da1c7f0d6e9c56d491bfc75656
kkatayama <katayama@udel.edu>
2021-02-10 14:12:30 -0500

    updated find_cmd

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='teddy',
-      version='0.92',
+      version='0.93',
       description='Teddy Utilities',
       url='http://github.com/kkatayama/teddy',
       author='Teddy',
