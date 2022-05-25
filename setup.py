d6eaf2d29a24dca34b8490ca1578a44981492b11
kkatayama <kkatayama@udel.edu>
2022-05-24 22:26:02 -0400

    Bump version: 1.0.7 → 1.0.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='teddy',
-      version='1.0.7',
+      version='1.0.8',
       description='Teddy Utilities',
       url='http://github.com/kkatayama/teddy',
       author='Teddy',
