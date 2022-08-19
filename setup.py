230451c736f6753f04ad1722708e2c25ae16f720
kkatayama <kkatayama@udel.edu>
2022-08-19 04:45:34 -0400

    Bump version: 1.0.10 → 1.0.11

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='teddy',
-      version='1.0.10',
+      version='1.0.11',
       description='Teddy Utilities',
       url='http://github.com/kkatayama/teddy',
       author='Teddy',
--- teddy/__init__.py
+++ teddy/__init__.py
@@ -27,7 +27,7 @@ from pathlib import Path
 
 # import markdown
 
-__version__ = "1.0.10"
+__version__ = "1.0.11"
 # -- CONFIGS -- #
 MODULE = coloredlogs.find_program_name()
 LOG_FILE = 'logs/{}.log'.format(os.path.splitext(MODULE)[0])
